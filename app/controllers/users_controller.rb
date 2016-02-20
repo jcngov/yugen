44ded115b81ca869d71a4d8f018dc45d3f575aa7
Jerry Ngov <jcngov@yahoo.com>
2016-02-20 14:49:47 -0800

    create users controller, new and index views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+def index
+  @users = User.all
+end
+
+def new
+  @user = User.new
+end
+
+def create
+  @user = User.new
+  flash[:notice] = "You Successfully Signed Up"
+  if @user.save
+    redirect_to users_path
+  else
+    render :new
+  end
+end
+
+end
