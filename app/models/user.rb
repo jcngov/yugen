fd149bbc96ac5f0a652e8bcdac9eb5030b33336f
Jerry Ngov <jcngov@yahoo.com>
2016-02-25 21:40:47 -0800

    allow users to edit their profile information

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :password, length: { minimum: 6 }
+  # validates :password, length: { minimum: 6 }
 
   has_attached_file :profilepicture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :profilepicture, content_type: /\Aimage\/.*\Z/
